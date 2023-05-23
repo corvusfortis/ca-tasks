115a960bb7d420415341b2313994cf31fe76350a
Denis Avidzba <avidzba.itrum@gmail.com>
2023-05-23 14:12:28 +0300

    solution: three-sum

--- index.js
+++ index.js
@@ -1,24 +1,33 @@
-var findMin = function(nums) {
-  let curDif = nums[1] - nums[0];
+var threeSum = function(nums) {
+  let result = [];
+  let temp = [];
+  let triplet;
   
-  
-  if(curDif < 0){
-      return nums[1] > nums[0] ? nums[0] : nums[1];
-  };
-  
-  let prevDif = 0;
-  for(let i = 1; i < nums.length; i++){
-      
-
-      
-      prevDif = curDif;
-      
-      curDif = nums[i] - nums[i - 1];
-      
-      if (curDif < 0){
-          return nums[i];
+  for(let i = 0; i < nums.length; i++){
+      for(let j = 0; j < nums.length; j++){
+          if(i === j){
+              continue;
+          }
+          for(let k = 0; k < nums.length; k++){
+              if(i === k || j === k){
+                  continue;
+              }
+             triplet = [];
+             triplet.push(nums[i]);
+             triplet.push(nums[j]);
+             triplet.push(nums[k]);
+             temp.push(triplet);
+          }
       }
   }
   
-  return nums[0];
+  temp = temp.map(e => e.sort((a, b) => a - b).join('|'));
+  
+  temp.forEach(e => {
+      if(!result.some(i => i === e)){
+          result.push(e);
+      }
+  })
+  
+  return result.map(e => e.split('|').map(i => Number(i))).filter(e => e.reduce((a, b) => a + b) === 0);
 };
