d5db5d5d78f17b45cfb6d6601cac10278f856c02
Denis Avidzba <avidzba.itrum@gmail.com>
2023-05-23 18:49:57 +0300

    fix: increase passed test number

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
 var lengthOfLongestSubstring = function(s) {
-    
   let subs = '';
-  let arr = [];
-  
-  for(let i = 0; i < s.length; i++){
-      if(!subs.includes(s[i])){
-          subs += s[i];
-      }else{
-          arr.push(subs);
-          subs = s[i];
+      let arr = [];
+      let j = 0;
+      
+      for(let i = 0; i < s.length; i++){
+          if(!subs.includes(s[i])){
+              subs += s[i];
+          }else{
+              arr.push(subs);
+              // s[i-1] === s[i] ? subs = s[i] : subs = s[i-1] + s[i];
+              subs = '';
+              j = i;
+              
+              while(!subs.includes(s[j]) && s[j - 1] !== s[j] && j >= 0){
+                  subs += s[j];
+                  j--;
+              }
+              
+              subs.split('').reverse().join('');
+          }
       }
-  }
-  
-  arr.push(subs);
-  return Math.max.apply(null, arr.map(e => e.length));
-};
+      
+      
+      console.log(arr);
+      return Math.max.apply(null, arr.map(e => e.length));
+  };
