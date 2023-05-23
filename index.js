7142f0a7a31d7a8343c589393748358ea6273941
Denis Avidzba <avidzba.itrum@gmail.com>
2023-05-23 16:23:41 +0300

    solution: spiral matrix

--- index.js
+++ index.js
@@ -1,24 +1,54 @@
-var setZeroes = function(matrix) {
+var spiralOrder = function(matrix) {
     
-  let zeroesI = [];
-  let zeroesJ = [];
+    
+  let height;
+  let width;
+  let result = [];
   
-  for(let i = 0; i < matrix.length; i++){
-      for(let j = 0; j < matrix[i].length; j++){
-          if(matrix[i][j] === 0){
-              zeroesI.push(i);
-              zeroesJ.push(j);
-          }
+  while(matrix.length > 1 && matrix[0].length > 1){
+      
+      height = matrix.length;
+      width = matrix[0].length;
+      
+      for(let i = 0; i < width; i++){
+          result.push(matrix[0][i]);
       }
+  
+  
+      for(let i = 1; i < height; i++){
+          result.push(matrix[i][width - 1]);
+      }
+      
+      for(let i = width - 2; i >= 0; i--){
+          result.push(matrix[height - 1][i]);
+      }
+      
+      
+      for(let i = height - 2; i > 0; i--){
+          result.push(matrix[i][0])
+      }
+      
+      matrix = trimMatrix(matrix);
+      
   }
   
-  for(let i = 0; i < matrix.length; i++){
-      for(let j = 0; j < matrix[i].length; j++){
-          if(zeroesI.some(e => e === i) || zeroesJ.some(e => e === j)){
-              matrix[i][j] = 0;
-          }
-      }
-  }
+  result.push(matrix);
   
-  return matrix;
+  
+
+  return result.flat(2);
 };
+                      
+                      
+                      
+function trimMatrix(m){
+  
+  m.shift();
+  m.pop();
+  m.forEach(e => {
+      e.shift();
+      e.pop();
+  })
+  
+  return m;
+}
