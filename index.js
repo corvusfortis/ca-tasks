630abf7b3ac61c4af190f36eed84806eaea2bb64
Denis Avidzba <avidzba.itrum@gmail.com>
2023-05-23 18:13:09 +0300

    solution: length of longest substring

--- index.js
+++ index.js
@@ -1,54 +1,17 @@
-var spiralOrder = function(matrix) {
+var lengthOfLongestSubstring = function(s) {
     
-    
-  let height;
-  let width;
-  let result = [];
+  let subs = '';
+  let arr = [];
   
-  while(matrix.length > 1 && matrix[0].length > 1){
-      
-      height = matrix.length;
-      width = matrix[0].length;
-      
-      for(let i = 0; i < width; i++){
-          result.push(matrix[0][i]);
+  for(let i = 0; i < s.length; i++){
+      if(!subs.includes(s[i])){
+          subs += s[i];
+      }else{
+          arr.push(subs);
+          subs = s[i];
       }
-  
-  
-      for(let i = 1; i < height; i++){
-          result.push(matrix[i][width - 1]);
-      }
-      
-      for(let i = width - 2; i >= 0; i--){
-          result.push(matrix[height - 1][i]);
-      }
-      
-      
-      for(let i = height - 2; i > 0; i--){
-          result.push(matrix[i][0])
-      }
-      
-      matrix = trimMatrix(matrix);
-      
   }
   
-  result.push(matrix);
-  
-  
-
-  return result.flat(2);
+  arr.push(subs);
+  return Math.max.apply(null, arr.map(e => e.length));
 };
-                      
-                      
-                      
-function trimMatrix(m){
-  
-  m.shift();
-  m.pop();
-  m.forEach(e => {
-      e.shift();
-      e.pop();
-  })
-  
-  return m;
-}
