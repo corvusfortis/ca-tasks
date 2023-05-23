1bb4ee7f45e96417348e89d1899621441d821a8b
Denis Avidzba <avidzba.itrum@gmail.com>
2023-05-23 11:29:02 +0300

    solution: valid-parantheses

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
-var isAnagram = function(s, t) {
+var maxSubArray = function(nums) {
     
-    return s.split('').sort().join('') === t.split('').sort().join('');
-}
+    if(nums.length === 1){
+        return nums[0];
+    }
+    
+    let temp = nums.sort((a, b) => b - a);
+    let currArr = [nums[0], nums[1]];
+    let currArrSum = nums[0] + nums[1];
+    let prevArrSum = 0;
+    
+    while(currArrSum > prevArrSum){
+        prevArrSum = currArrSum;
+        currArr.push(temp[currArr.length]);
+        
+        
+        currArrSum = currArr.reduce((a, b) => a + b);
+    }
+    
+    return currArr;
+};
