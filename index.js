d3dacd02e1909c072f59bcc38ad6c029339c3944
Denis Avidzba <avidzba.itrum@gmail.com>
2023-05-23 12:55:08 +0300

    solution: find minimum in rotated sorted array

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
-var maxSubArray = function(nums) {
-    
-    if(nums.length === 1){
-        return nums[0];
-    }
-    
-    let temp = nums.sort((a, b) => b - a);
-    let currArr = [nums[0], nums[1]];
-    let currArrSum = nums[0] + nums[1];
-    let prevArrSum = 0;
-    
-    while(currArrSum > prevArrSum){
-        prevArrSum = currArrSum;
-        currArr.push(temp[currArr.length]);
-        
-        
-        currArrSum = currArr.reduce((a, b) => a + b);
-    }
-    
-    return currArr;
+var findMin = function(nums) {
+  let curDif = nums[1] - nums[0];
+  
+  
+  if(curDif < 0){
+      return nums[1] > nums[0] ? nums[0] : nums[1];
+  };
+  
+  let prevDif = 0;
+  for(let i = 1; i < nums.length; i++){
+      
+
+      
+      prevDif = curDif;
+      
+      curDif = nums[i] - nums[i - 1];
+      
+      if (curDif < 0){
+          return nums[i];
+      }
+  }
+  
+  return nums[0];
 };
