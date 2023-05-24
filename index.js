e056dafae95505bba6696a4f9673c4283c1960bf
Denis Avidzba <avidzba.itrum@gmail.com>
2023-05-24 10:49:22 +0300

    fix: wrong task code

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-var isAnagram = function(s, t) {
-    
-    return s.split('').sort().join('') === t.split('').sort().join('');
-}
+var isPalindrome = function(s) {
+  let result = s.toLowerCase().split('').filter(e => /[a-z0-9]/.test(e));
+  
+  let firstHalf = result.slice(0, Math.floor(result.length / 2)).join('');
+  
+  let secondHalf = result.slice(Math.ceil(result.length / 2) ).reverse().join('');
+  
+  
+  return firstHalf === secondHalf;
+};
