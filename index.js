55d9e91510f3fc5a38c95eee599ab99db57ee9b2
Denis Avidzba <avidzba.itrum@gmail.com>
2023-05-24 10:41:41 +0300

    fix: wrong task code

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
-var isAnagram = function(s, t) {
-    
-    return s.split('').sort().join('') === t.split('').sort().join('');
-}
+var productExceptSelf = function(nums) {
+  let result = [];
+  let temp;
+  
+  for (let i = 0; i < nums.length; i++){
+      temp = [...nums];
+      
+      temp.splice(i, 1);
+      
+      result.push(temp.reduce((a, b) => a * b));
+      
+  }
+  
+  return result;
+};
