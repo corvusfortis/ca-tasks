03a944a6df68754b6a9383d7757db3fc4d2e36bd
Denis Avidzba <avidzba.itrum@gmail.com>
2023-05-26 11:31:25 +0300

    solution: search-in-rotated-array

--- index.js
+++ index.js
@@ -1,26 +1,51 @@
-var rotate = function(m) {
-  m = transpose(m);
-  m = reverse(m);
-};
+function search(nums, target){
+  let mid = findMin(nums);
+  return binary(nums, target, mid);
+}
 
-function transpose(m){
-  let temp = [];
+function findMin(nums) {
+  let curDif = nums[1] - nums[0];
   
-  m.forEach(e => {
-      temp.push(Array(3).fill(0));
-  })
   
-  for(let i = 0; i < m.length; i++){
-      for(let j = 0; j < m[0].length; j++){
-          temp[i][j] = m[j][i];
+  if(curDif < 0){
+      return nums[1] > nums[0] ? nums[0] : nums[1];
+  };
+  
+  let prevDif = 0;
+  for(let i = 1; i < nums.length; i++){
+      
+      prevDif = curDif;
+      
+      curDif = nums[i] - nums[i - 1];
+      
+      if (curDif < 0){
+          return nums[i];
       }
   }
   
-  m = temp;
-  return m;
+  return nums[0];
+};
+
+function binary(nums, target, mid) {
+let start = 0;
+let end = nums.length - 1;
+let center = mid;
+
+while(start <= end){
+     
+     
+     if(nums[center] === target){
+         return center;
+     }
+     
+     if(nums[center] < target){
+         start = center + 1;
+     }else{
+         end = center - 1;
+     }
+     
+     center = Math.floor((start + end) / 2);
 }
 
-function reverse(m){
-  m.forEach(e => e.reverse());
-  return m;
-}
+return -1;
+};
