f1ccca86cbd032ac9c52a21398b8fa2710450a86
Denis Avidzba <avidzba.itrum@gmail.com>
2023-05-26 14:16:10 +0300

    solution: merge-intervals

--- index.js
+++ index.js
@@ -1,49 +1,17 @@
-var exist = function(board, word) {
-  let firstLetterIJ = findFirstLetter(board, word[0]);
+var merge = function(intervals) {
+  let mergeCounter;
+  let temp = [...intervals];
   
-  if(firstLetterIJ === null){
-      return false;
-  }
-  
-  let currLetterIJ = firstLetterIJ;
-  
-  for(let i = 1; i < word.length; i++){
-      console.log(currLetterIJ);
-      currLetterIJ = checkNextLetter(board, currLetterIJ, word[i]);
-      
-      if(currLetterIJ === null){
-          return false;
+  while(mergeCounter !== 0){
+      mergeCounter = 0;
+      for(let i = 1; i < temp.length; i++){
+          if(temp[i - 1][1] >= temp[i][0]){
+              mergeCounter++;
+              temp[i - 1][1] = temp[i][1];
+              temp.splice(i, 1);
+              break;
+          }
       }
   }
-  
-  return true;
+  return temp;
 };
-
-function findFirstLetter(board, letter){
-  let j;
-  for(let i = 0; i < board.length; i++){
-      j = board[i].indexOf(letter);
-      if(j !== -1){
-          return [i, j];
-      }
-  }
-  return null;
-}
-
-function checkNextLetter(board, prevIJ, letter){
-  let [i, j] = prevIJ;
-  
-  let ijs = [[i-1, j], [i+1, j], [i, j-1], [i, j+1]];
-  ijs = ijs.filter(e => !e.some(p => p < 0) && !e.some(p => p >= board.length));
-  
-  
-  for(let k = 0; k < ijs.length; k++){
-      let [first, second] = ijs[k];
-      
-      if(board[first][second] === letter){
-          return [first, second]
-      }
-
-  }
-    return null;
-}
