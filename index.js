e577087e9c70c581633a19000bdc29525c631b54
Denis Avidzba <avidzba.itrum@gmail.com>
2023-05-26 14:28:57 +0300

    fix: edge cases

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 var merge = function(intervals) {
   let mergeCounter;
   let temp = [...intervals];
+  temp.sort((a, b) => a[0] - b[0]);
   
   while(mergeCounter !== 0){
       mergeCounter = 0;
       for(let i = 1; i < temp.length; i++){
-          if(temp[i - 1][1] >= temp[i][0]){
+          if(temp[i - 1][1] >= temp[i][0] || temp[i-1][1] >= temp[i][1]){
               mergeCounter++;
-              temp[i - 1][1] = temp[i][1];
+              temp[i - 1][1] = Math.max(temp[i][1], temp[i-1][1]);
               temp.splice(i, 1);
               break;
           }
