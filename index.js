f87e2d29efe2d2acc8d1532fc1c6b5033453520c
Denis Avidzba <avidzba.itrum@gmail.com>
2023-05-26 10:56:03 +0300

    solution: rotate matrix

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
-var hasCycle = function(head) {
-  let vals = [];
-  let curr = head;
-
-  while(curr !== null){
-
-     if(vals.some(e => e === curr.val)){
-         return true;
-     }
-
-      vals.push(curr.val);
-      curr = curr.next;
-
-  }
-
-  return false;
-
+var rotate = function(m) {
+  m = transpose(m);
+  m = reverse(m);
 };
+
+function transpose(m){
+  let temp = [];
+  
+  m.forEach(e => {
+      temp.push(Array(3).fill(0));
+  })
+  
+  for(let i = 0; i < m.length; i++){
+      for(let j = 0; j < m[0].length; j++){
+          temp[i][j] = m[j][i];
+      }
+  }
+  
+  m = temp;
+  return m;
+}
+
+function reverse(m){
+  m.forEach(e => e.reverse());
+  return m;
+}
